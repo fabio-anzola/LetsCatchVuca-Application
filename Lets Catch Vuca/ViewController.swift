9133621be18014436a6e2dc53960953340822632
Anzola Fabio <anzola.fabio@gmail.com>
2019-06-20 15:50:31 +0200

    Added a current tab Label

--- Lets Catch Vuca/ViewController.swift
+++ Lets Catch Vuca/ViewController.swift
@@ -9,23 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var currentTab: UILabel!
+    
     override func viewDidLoad() {
         Thread.sleep(forTimeInterval: 1.0)
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     @IBAction func homeButton(_ sender: Any) {
-        print("home pressed")
+//        print("home pressed")
+        currentTab.text = "Home"
     }
     @IBAction func blogsButton(_ sender: Any) {
-        print("blogs pressed")
+//        print("blogs pressed")
+        currentTab.text = "Blogs"
     }
     @IBAction func loginButton(_ sender: Any) {
-        print("login pressed")
+//        print("login pressed")
+        currentTab.text = "Login"
     }
     @IBAction func infoButton(_ sender: Any) {
-        print("info pressed")
+//        print("info pressed")
+        currentTab.text = "Info"
     }
     
 
