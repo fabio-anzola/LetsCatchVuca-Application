a574f07455726d866b1e151e899bec23ca3a7c64
Anzola Fabio <anzola.fabio@gmail.com>
2019-06-21 19:50:48 +0200

    Added function of buttons, buttons now open different webpages

--- Lets Catch Vuca/ViewController.swift
+++ Lets Catch Vuca/ViewController.swift
@@ -14,23 +14,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var currentTab: UILabel!
     @IBOutlet weak var webScreen: WKWebView!
     
+    let requestHome = URLRequest(url: URL(string: "https://www.catchvuca.com/")!)
+    let requestBlog = URLRequest(url: URL(string: "https://www.catchvuca.com/aktuelle-blogs/")!)
+    let requestLogin = URLRequest(url: URL(string: "https://www.catchvuca.com/dashboard/")!)
+    
     override func viewDidLoad() {
         Thread.sleep(forTimeInterval: 1.0)
         super.viewDidLoad()
-        let request = URLRequest(url: URL(string: "https://catchvuca.com")!)
-        webScreen?.load(request)
+        webScreen?.load(requestHome)
         // Do any additional setup after loading the view.
     }
     @IBAction func homeButton(_ sender: Any) {
 //        print("home pressed")
+        webScreen?.load(requestHome)
         currentTab.text = "Home"
     }
     @IBAction func blogsButton(_ sender: Any) {
 //        print("blogs pressed")
+        webScreen?.load(requestBlog)
         currentTab.text = "Blogs"
     }
     @IBAction func loginButton(_ sender: Any) {
 //        print("login pressed")
+        webScreen?.load(requestLogin)
         currentTab.text = "Login"
     }
     @IBAction func infoButton(_ sender: Any) {
